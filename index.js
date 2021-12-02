176677a93df9bad6f3692819512001771d59688e
Michael Hu <pxmyhdev@gmail.com>
2021-12-01 23:29:42 -0500

    click first answer of daily poll

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const puppeteer = require('puppeteer');
 const C = require('./constants');
-const LOGIN_SELECTOR =
+
+// variable declaration
+const loginLinkSelector =
   'body > div.container-fliud > div.dark > header > div:nth-child(2) > div.br-main-menu.float-right > ul > li:nth-child(2)';
-const USERNAME_SELECTOR = '#UserEmail';
-const PASSWORD_SELECTOR = '#UserPassword';
-const SUBMIT_SELECTOR = '#UserLoginForm > div:nth-child(4) > div > div > input';
-const REDIRECTION_SELECTOR = '#info > a';
+const usernameSelector = '#UserEmail';
+const passwordSelector = '#UserPassword';
+const loginSubmitSelector =
+  '#UserLoginForm > div:nth-child(4) > div > div > input';
 const mintvineDashboardUrl = 'https://surveys.gobranded.com/users/dashboard';
-const poll_wrapper_selector = '#PollDashboardForm > div.poll-ui-wrapper';
+const pollWrapperSelector =
+  '#PollDashboardForm > div.poll-ui-wrapper > label.poll-label';
+const pollAnswerSubmitSelector =
+  '#PollDashboardForm > input.submit-poll.d-none.d-sm-flex.btn.btn-outline-primary';
+const screenshotName = 'mintvine.png';
 
 startBrowser = async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, dumpio: true });
   const page = await browser.newPage();
   return { browser, page };
 };
@@ -26,28 +32,44 @@ login = async (url) => {
   // login
   await page.goto(url, { waitUntil: 'load' });
   console.log('Mintvine page loaded');
-  await page.click(LOGIN_SELECTOR);
-  await page.waitForTimeout(4000);
-  await page.click(USERNAME_SELECTOR);
+  await page.click(loginLinkSelector);
+  await page.waitForTimeout(2000);
+  await page.click(usernameSelector);
   await page.keyboard.type(C.username);
-  await page.click(PASSWORD_SELECTOR);
+  await page.click(passwordSelector);
   await page.keyboard.type(C.password);
-  await page.click(SUBMIT_SELECTOR);
+  await page.click(loginSubmitSelector);
   console.log('Submitted login info');
   await page.waitForNavigation();
 
   // dashboard
   await page.goto(mintvineDashboardUrl, { waitUntil: 'load' });
   console.log('Mintvine dashboard loaded');
-  await page.evaluate(() => {
-    // let elements = $(poll_wrapper_selector).toArray();
-    let elements = $('#PollDashboardForm > div.poll-ui-wrapper').toArray();
-    for (i = 0; i < elements.length; i++) {
-      console.log('element is ', elements[i]);
-      // $(elements[i]).click();
+
+  await page.evaluate((pollWrapperSelector) => {
+    let elements = $(pollWrapperSelector).toArray();
+    // let elements = $(
+    //   '#PollDashboardForm > div.poll-ui-wrapper > label.poll-label'
+    // ).toArray();
+    console.log('elements is ', elements);
+    if (elements.length != 0) {
+      $(elements[0]).click(); // click the first element
     }
+
+    // alternative: click each element, just for fun :)
+    // for (i = 0; i < elements.length; i++) {
+    //   console.log('element is ', elements[i]);
+    //   $(elements[i]).click();
+    // }
   });
-  await page.screenshot({ path: 'mintvine.png', fullPage: true });
+  await page.click(pollAnswerSubmitSelector);
+  console.log('Submitted daily poll answer');
+
+  // taking a screenshot
+  // TODO: set screenshot name with date time
+  await page.screenshot({ path: screenshotName, fullPage: true });
+  await page.waitForTimeout(200000000);
+
   browser.close();
 };
 
